034cfece7d512ab016b35e1723f70e44d51b3cdb
AshuaiNe <2818820548@qq.com>
2020-11-04 17:58:06 +0800

    优化

--- page/main.py
+++ page/main.py
@@ -4,7 +4,7 @@ Version: 1.0
 Author: Penn
 Date: 2020-05-21 19:49:43
 LastEditors: Penn
-LastEditTime: 2020-10-30 17:46:09
+LastEditTime: 2020-11-04 17:57:52
 '''
 from page.address_book import AddressBook
 from page.base_page import BasePage
@@ -15,5 +15,4 @@ class Main(BasePage):
 
     def goto_address_book(self):
         self.steps("../data/main.yaml")
-        # self.find(By.ID, "menu_contacts").click()
         return AddressBook(self._driver)
